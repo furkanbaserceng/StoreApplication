87bf1a42d02a04cb266f56876cdf76b04e665a40
furkanbaserceng@outlook.com <furkanbaserceng@outlook.com>
2023-06-09 12:27:07 +0300

    Basket operations were designed according to Pages structure.

---
+++ Entities/Models/Cart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Cart
+    {
+        public List<CartLine> Lines { get; set; }
+
+        public Cart()
+        {
+            Lines=new List<CartLine>();
+        }
+
+        public virtual void AddItem(Product product,int quantity)
+        {
+            CartLine? line=Lines.Where(l=>l.Product.ProductId==product.ProductId).FirstOrDefault();
+            if(line is null)
+            {
+                Lines.Add(new CartLine() { Product = product,Quantity = quantity});
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product)=>Lines.RemoveAll(l=>l.Product.ProductId.Equals(product.ProductId));
+
+        public void Clear() => Lines.Clear();
+        public decimal ComputeTotalValue() => Lines.Sum(e => e.Product.Price * e.Quantity);
+
+
+
+    }
+}
--- StoreApp/Pages/Cart.cshtml.cs
+++ StoreApp/Pages/Cart.cshtml.cs
@@ -1,12 +1,42 @@
+using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Contracts;
 
 namespace StoreApp.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+        private readonly IServiceManager _manager;
+        public Cart Cart { get; set; } 
+        public string ReturnUrl { get; set; } = "/";
+
+        public CartModel(IServiceManager manager,Cart cart)
         {
+            _manager = manager;
+            Cart = cart;
         }
+
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+        }
+
+        public IActionResult OnPost(int productId,string returnUrl)
+         {
+            Product? product = _manager.ProductService.Get(productId, false);
+            if(product is not null)
+            {
+                Cart.AddItem(product, 1);
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostRemove(int id,string returnUrl)
+        {
+            Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductId.Equals(id)).Product);
+            return Page();
+        }
+
     }
 }
