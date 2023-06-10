7b1951179985b203c1c29c0dc920394f71be41ca
furkanbaserceng@outlook.com <furkanbaserceng@outlook.com>
2023-06-10 17:03:21 +0300

    The URL content that holds the information from whichever page the cart came from was captured by the HttpRequestExtension method and returned.

---
+++ StoreApp/Infrastructure/Extensions/HttpRequestExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace StoreApp.Infrastructure.Extensions
+{
+    public static class HttpRequestExtension
+    {
+
+        public static string PathAndQuery(this HttpRequest request) 
+        {
+
+
+            return request.QueryString.HasValue ? $"{request.Path}{request.QueryString}":request.Path.ToString();
+        
+        
+        }
+
+    }
+}
