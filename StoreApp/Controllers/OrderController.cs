37e8e9f49511c541831d9045704c213123548517
furkanbaserceng@outlook.com <furkanbaserceng@outlook.com>
2023-06-11 22:03:56 +0300

    Order details continued.

---
+++ StoreApp/Controllers/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IServiceManager _manager;
+        private readonly Cart _cart;
+
+        public OrderController(IServiceManager manager, Cart cart)
+        {
+            _manager = manager;
+            _cart = cart;
+        }
+
+        [HttpGet]
+        public ViewResult Checkout()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Checkout([FromForm] Order order)
+        {
+            if (_cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "sorry,your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cart.Lines.ToArray();
+                _manager.OrderService.SaveOrder(order);
+                _cart.Clear();
+                return RedirectToPage("/Complete", new {OrderId=order.OrderId});
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
