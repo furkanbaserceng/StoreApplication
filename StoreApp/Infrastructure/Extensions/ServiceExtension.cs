ceadb54f8e5017b4751e29eae9e984e84b75574e
furkanbaserceng@outlook.com <furkanbaserceng@outlook.com>
2023-06-12 23:31:53 +0300

    Added ServiceExtension and refactored related codes.

---
+++ StoreApp/Infrastructure/Extensions/ServiceExtension.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Contracts;
+using Services.Contracts;
+using Services;
+using Entities.Models;
+using StoreApp.Models;
+
+namespace StoreApp.Infrastructure.Extensions
+{
+    public static class ServiceExtension
+    {
+
+
+        public static void ConfigureDbContext(this IServiceCollection services,IConfiguration configuration)
+        {
+
+            services.AddDbContext<RepositoryContext>(options =>
+            {
+
+                options.UseSqlServer(configuration.GetConnectionString("mssqlserverconnection"),
+                    b => b.MigrationsAssembly("StoreApp"));
+                //options.EnableSensitiveDataLogging();
+
+            });
+
+
+        }
+
+        public static void ConfigureSession(this  IServiceCollection services) 
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = "StoreApp.Session";
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+            });
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<Cart>(c => SessionCart.GetCart(c));
+
+        }
+
+        public static void ConfigureRepositoryRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+        }
+
+        public static void ConfigureServiceRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IServiceManager, ServiceManager>();
+            services.AddScoped<IProductService, ProductManager>();
+            services.AddScoped<ICategoryService, CategoryManager>();
+            services.AddScoped<IOrderService, OrderManager>();
+        }
+
+
+
+
+      
+    }
+}
--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -1,10 +1,5 @@
-using Entities.Models;
-using Microsoft.EntityFrameworkCore;
-using Repositories;
-using Repositories.Contracts;
-using Services;
-using Services.Contracts;
-using StoreApp.Models;
+using StoreApp.Infrastructure.Extensions;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,41 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
-
-builder.Services.AddDbContext<RepositoryContext>(options =>
-{
-
-    options.UseSqlServer(builder.Configuration.GetConnectionString("mssqlserverconnection"),
-        b=>b.MigrationsAssembly("StoreApp"));
-    //options.EnableSensitiveDataLogging();
-    
-});
-
-builder.Services.AddDistributedMemoryCache();
-builder.Services.AddSession(options =>
-{
-    options.Cookie.Name = "StoreApp.Session";
-    options.IdleTimeout = TimeSpan.FromMinutes(10);
-});
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-
-builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
-builder.Services.AddScoped<IProductRepository,ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
-builder.Services.AddScoped<IOrderRepository,OrderRepository>();
-
-builder.Services.AddScoped<IServiceManager, ServiceManager>();
-builder.Services.AddScoped<IProductService,ProductManager>();
-builder.Services.AddScoped<ICategoryService,CategoryManager>();
-builder.Services.AddScoped<IOrderService,OrderManager>();
-
-builder.Services.AddScoped<Cart>(c => SessionCart.GetCart(c));
-
+builder.Services.ConfigureDbContext(builder.Configuration);
+builder.Services.ConfigureSession();
+builder.Services.ConfigureRepositoryRegistration();
+builder.Services.ConfigureServiceRegistration();
 
 builder.Services.AddAutoMapper(typeof(Program));
-
-
 
 
 var app = builder.Build();
