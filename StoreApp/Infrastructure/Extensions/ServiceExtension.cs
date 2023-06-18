3f7150692f78fa54fcf096ad859807d4fe9dc2f3
furkanbaserceng@outlook.com <furkanbaserceng@outlook.com>
2023-06-18 22:22:41 +0300

    Lowercase plugin has been written in the application's routing settings.

--- StoreApp/Infrastructure/Extensions/ServiceExtension.cs
+++ StoreApp/Infrastructure/Extensions/ServiceExtension.cs
@@ -57,7 +57,16 @@ namespace StoreApp.Infrastructure.Extensions
             services.AddScoped<IOrderService, OrderManager>();
         }
 
+        public static void ConfigureRouting(this IServiceCollection services)
+        {
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = false; //also default false
 
+            });
+
+        }
 
 
       
